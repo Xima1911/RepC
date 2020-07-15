eb638fe4c13c32343ca16c9ea78eb74e92e06607
Xima1911 <59200668+Xima1911@users.noreply.github.com>
2020-07-15 11:24:01 +0200

    zurückgesetzt auf n =4?

--- Testdatei.py
+++ Testdatei.py
@@ -6,5 +6,15 @@ def function(x):
     return a
 
 
+<<<<<<< HEAD
 n = 10
+=======
+n = 2
+print ("a = 10 + %d"%n,": ", function(n))
+
+n = 3
+print ("a = 10 + %d"%n,": ", function(n))
+
+n = 4
+>>>>>>> parent of a88dd02... Revert "Testdatei: n = 4"
 print ("a = 10 + %d"%n,": ", function(n))
